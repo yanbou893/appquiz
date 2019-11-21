19166cf71e1d6f3bb9630e87380b1bb78f052c3a
EC2 Default User <ec2-user@ip-172-31-84-70.ec2.internal>
2019-11-21 06:23:11 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import Home from './containers/Home';
+
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const NotFound = () => {
+  return(
+    <h2>ページが見つかりません</h2>
+  )
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      <Switch>
+      <Route exact path="/" component={Home} />
+      </Switch>
+      );
+  }
 }
 
 export default App;
---
+++ src/containers/Home.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+import Search from '@material-ui/icons/Search';
+
+import * as actions from '../actions';
+
+const styles = theme => ({
+  titleImage: {
+    width: '100%',
+    maxWidth: 700,
+  },
+  
+  button: {
+    marginTop: 30,
+    marginBottom: 20,
+    fontSize: 16,
+    padding: 10,
+    width: 250,
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
+  },
+  root: {
+  },
+
+  // Form
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
+});
+
+class Home extends React.Component {
+    render() {
+    // redux関連
+    const { actions } = this.props;
+    // Material-ui関連
+    const { classes } = this.props;
+        return (
+        <div>
+          <p>Quiz</p>
+          <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={() => actions.getAnimes(this.state.year, this.state.cour)}
+          >Start
+          </Button>
+        </div>
+        );
+    }
+}
+
+// Material-ui関連
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
