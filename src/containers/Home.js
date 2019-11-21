fc43f2b7b2bb775bb4778a5d2fcd359cfd8545c6
EC2 Default User <ec2-user@ip-172-31-84-70.ec2.internal>
2019-11-21 06:35:15 +0000

    secoundhome

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-import Search from '@material-ui/icons/Search';
 
 
 const styles = theme => ({
@@ -36,24 +35,13 @@ const styles = theme => ({
 
 class Home extends React.Component {
     render() {
-    // Material-ui関連
-    const { classes } = this.props;
         return (
         <div>
           <p>Quiz</p>
-          <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          >Start
+          <Button>
+          Start
           </Button>
         </div>
         );
     }
 }
-
-// Material-ui関連
-Home.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
-};
